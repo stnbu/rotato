b44b55aae595d5fc566be198dcb5da53adc8be2c
Mike Burr <mb@unintuitive.org>
2023-04-14 11:30:48 -0600

    x

--- src/debug_lines.rs
+++ src/debug_lines.rs
@@ -2,7 +2,36 @@ use bevy::prelude::*;
 use bevy_prototype_debug_lines::DebugLines;
 
 pub fn debug_lines(mut lines: ResMut<DebugLines>) {
-    lines.line_colored(Vec3::X * 20.0, Vec3::X * -20.0, 0.0, Color::RED);
-    lines.line_colored(Vec3::Y * 20.0, Vec3::Y * -20.0, 0.0, Color::GREEN);
-    lines.line_colored(Vec3::Z * 20.0, Vec3::Z * -20.0, 0.0, Color::BLUE);
+    const AXIS_EXTENT: f32 = 10.0;
+    const TICK_EXTENT: f32 = 0.1;
+
+    lines.line_colored(
+        Vec3::X * AXIS_EXTENT,
+        Vec3::X * -AXIS_EXTENT,
+        0.0,
+        Color::RED,
+    );
+    lines.line_colored(
+        Vec3::Y * AXIS_EXTENT,
+        Vec3::Y * -AXIS_EXTENT,
+        0.0,
+        Color::GREEN,
+    );
+    lines.line_colored(
+        Vec3::Z * AXIS_EXTENT,
+        Vec3::Z * -AXIS_EXTENT,
+        0.0,
+        Color::BLUE,
+    );
+
+    let tick_count = AXIS_EXTENT.floor() as i32;
+
+    for distance in 1..tick_count {
+        for side in [-1, 1] {
+            let distance = (side * distance) as f32;
+            let from = Vec3::new(distance, TICK_EXTENT, 0.0);
+            let to = Vec3::new(distance, -TICK_EXTENT, 0.0);
+            lines.line_colored(from, to, 0.0, Color::RED);
+        }
+    }
 }
