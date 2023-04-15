ad3ff05ebe5f8252a6cb5ea4b22e87c6802d68e2
Mike Burr <mb@unintuitive.org>
2023-04-14 22:49:44 -0600

    YYYY

--- src/gui.rs
+++ src/gui.rs
@@ -13,11 +13,18 @@ pub fn menu(
         *pending_parameters = *parameters;
     }
     egui::Window::new("Configuration...").show(contexts.ctx_mut(), |ui| {
+        ui.label("\"b\" + mouse = rotate camera [b]oom about origin");
+        ui.label("\"g\" + mouse = [g]imbal camera (look around)");
+        ui.label("SPACE = (un)pause rotation ");
+        ui.add(egui::Separator::default());
+        // -
         ui.add(egui::Slider::new(&mut pending_parameters.y_bias, 0.0..=7.0).text("Y-bias"));
         ui.add(
             egui::Slider::new(&mut pending_parameters.max_children, 1..=10).text("Max Children"),
         );
         ui.add(egui::Slider::new(&mut pending_parameters.max_depth, 1..=10).text("Max Depth"));
+
+        // Colors: how do they work?
 
         ui.horizontal(|ui| {
             let [r, g, b, a] = pending_parameters.color_endpoints.0.as_rgba_f32();
@@ -31,12 +38,20 @@ pub fn menu(
             let [r, g, b, a] = rgba.to_array();
             pending_parameters.color_endpoints.0 = Color::rgba(r, g, b, a);
         });
-        // let mut color2 = [1.0, 0.0, 0.0];
-        // ui.horizontal(|ui| {
-        //     ui.label("End Color:");
-        //     egui::color_picker::color_edit_button_rgb(ui, &mut color2);
-        // });
-        // -
+
+        ui.horizontal(|ui| {
+            let [r, g, b, a] = pending_parameters.color_endpoints.1.as_rgba_f32();
+            let mut rgba = egui::Rgba::from_rgba_unmultiplied(r, g, b, a);
+            ui.label("End Color:");
+            egui::color_picker::color_edit_button_rgba(
+                ui,
+                &mut rgba,
+                egui::color_picker::Alpha::BlendOrAdditive,
+            );
+            let [r, g, b, a] = rgba.to_array();
+            pending_parameters.color_endpoints.1 = Color::rgba(r, g, b, a);
+        });
+        ui.add(egui::Separator::default());
         if ui.add(egui::Button::new("Apply")).clicked() {
             *parameters = *pending_parameters;
         }
